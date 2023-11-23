8a97a65852b29fdd7c3d7068744c38563d28aa0b
Mohamed Zain <mzain@TradeNET.COM>
2023-11-23 11:42:18 +0200

    swagger

--- src/main/java/com/example/shopping_cart_second/ShoppingCartSecondApplication.java
+++ src/main/java/com/example/shopping_cart_second/ShoppingCartSecondApplication.java
@@ -2,10 +2,9 @@ package com.example.shopping_cart_second;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-//@EnableCaching
+//@EnableSwagger2
 public class ShoppingCartSecondApplication {
 
 	public static void main(String[] args) {
